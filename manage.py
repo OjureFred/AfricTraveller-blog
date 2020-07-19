b3c63a1ccbec56906e9883e8c2a87547d6fee82e
Fred Ojure <fredojure@hotmail.com>
2020-07-19 10:56:39 +0300

    Coded unit tests for Blog class

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 from app.models import Pitch, User, Role
 
-app = create_app('production')
+app = create_app('develoment')
 migrate = Migrate(app, db)
 
 manager = Manager(app)

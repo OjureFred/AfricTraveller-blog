10936e95d2536d371faadcc5f7d48553de001b92
Fred Ojure <fredojure@hotmail.com>
2020-07-21 19:07:40 +0300

    Coded registration route and form

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,20 @@
 from flask import render_template
+from ..models import User
+from .forms import RegistrationForm
+from .. import db
 from . import auth
 
 @auth.route('/login')
 def login():
     return render_template('auth/login.html')
+    pass
+
+@auth.route('/register', methods=["GET", "post"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data, username=form.username.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        title = "New Account"
+    return render_template('/auth/register.html', registration_form = form)

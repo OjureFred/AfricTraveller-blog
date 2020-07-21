785907ca4fd1d4fba832979b8857287be8edf6b4
Fred Ojure <fredojure@hotmail.com>
2020-07-21 21:52:16 +0300

    Completed the Welcome email process

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,6 +4,7 @@ from ..models import User
 from .forms import LoginForm, RegistrationForm
 from .. import db
 from . import auth
+from ..email import mail_message
 
 @auth.route('/login', methods = ['GET', 'POST'])
 def login():
@@ -27,7 +28,11 @@ def register():
         user = User(email=form.email.data, username=form.username.data, password=form.password.data)
         db.session.add(user)
         db.session.commit()
-        title = "New Account"
+
+        mail_message("Welcome to Afric-Travel Blog", "email/welcome_user", user.email, user=user)
+        
+        return redirect(url_for('auth.login'))
+        title = "New User Account"
     return render_template('/auth/register.html', registration_form=form)
 
 @auth.route('/logout')
--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 set SECRET_KEY=127654
 set MAIL_USERNAME=pythonpjs@gmail.com
-SET MAIL_PASSWORD =11>Nyasaye99
+SET MAIL_PASSWORD =pythonpjs
 
 virtual\Scripts\python manage.py server

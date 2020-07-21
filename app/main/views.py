2d4d6ef46ccdd253f215361b4fa7597492195f72
Fred Ojure <fredojure@hotmail.com>
2020-07-21 16:25:16 +0300

    Configured login

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,6 @@
 from flask import render_template, request, redirect, url_for
 from . import main
+from flask_login import login_required
 #from .forms import CommentForm
 from ..models import Comment
 
@@ -8,4 +9,12 @@ from ..models import Comment
 def index():
     '''
     Function to load the index page
+    '''
+    pass
+
+@main.route('/blog/comment/new/<int:id>', methods=['GET', 'POST'])
+@login_required
+def new_comment(id):
+    '''
+    Function to get a new comment
     '''
